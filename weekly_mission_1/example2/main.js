3726f2aae411b16fe2829b5f38e9bc6deea0e3f5
Rebeca Cuevas <rebecaacuevasa@gmail.com>
2022-04-09 22:43:26 -0500

    example 1 and 2

---
+++ weekly_mission_1/example2/main.js
@@ -0,0 +1,8 @@
+// node main.js
+
+// Exportando funciones entre scripts con CommonJS
+
+const logger = require('./logger')
+
+logger.info('This is an informational message')
+logger.verbose('This is a verbose message')
